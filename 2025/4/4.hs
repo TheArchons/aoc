2807ce75b193e56ff1153327b1254a208fee3a2a
TheArchons <github@thearchons.xyz>
2025-12-29 10:11:35 -0500

    4 complete
    Not my cleanest work but it gets the job done

--- 2025/4/4.hs
+++ 2025/4/4.hs
@@ -1,6 +1,6 @@
 main = do
   contents <- getContents
-  print (numRolls (parseContents contents))
+  print (numRemovable (parseContents contents))
 
 between :: (Ord a) => a -> a -> a -> Bool
 between a b c = a < b && b < c
@@ -11,14 +11,25 @@ isAccessible row col rolls =
     [fromEnum (rolls !! (row + r) !! (col + c)) | r <- [-1, 0, 1], between (-1) (row + r) (length rolls), c <- [-1, 0, 1], between (-1) (col + c) (length (head rolls)), r /= 0 || c /= 0]
     < 4
 
-numRolls :: [[Bool]] -> Int
-numRolls rolls =
-  sum
-    [ fromEnum (isAccessible row col rolls)
-      | row <- [0 .. (length rolls - 1)],
-        col <- [0 .. (length (head rolls) - 1)],
-        rolls !! row !! col
-    ]
+findRemovable :: [[Bool]] -> [(Int, Int)]
+findRemovable rolls =
+  [ (row, col)
+    | row <- [0 .. (length rolls - 1)],
+      col <- [0 .. (length (head rolls) - 1)],
+      rolls !! row !! col,
+      isAccessible row col rolls
+  ]
+
+remove :: [[Bool]] -> [(Int, Int)] -> [[Bool]]
+remove rolls removable = [[col && (rowIdx, colIdx) `notElem` removable | (col, colIdx) <- zip row [0 ..]] | (row, rowIdx) <- zip rolls [0 ..]]
+
+numRemovable :: [[Bool]] -> Int
+numRemovable rolls =
+  if not (null removable)
+    then length removable + numRemovable (remove rolls removable)
+    else 0
+  where
+    removable = findRemovable rolls
 
 parseContents :: String -> [[Bool]]
 parseContents contents = [[c == '@' | c <- row] | row <- words contents]
