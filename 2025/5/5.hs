f02379514560701c30be2bfb20f7db9456ff4d0c
TheArchons <github@thearchons.xyz>
2025-12-30 14:58:33 -0500

    5 complete

--- 2025/5/5.hs
+++ 2025/5/5.hs
@@ -4,17 +4,21 @@ main = do
   contents <- getContents
   print (numFreshIds (parseInput contents))
 
+numInRange :: Int -> Int -> Int
 numInRange start end
   | end - start < 0 = 0
   | otherwise = end - start + 1
 
+calculateStart :: Int -> [(Int, Int)] -> Int -> Int
 calculateStart start ranges idx
   | idx == 0 = start
   | otherwise = max start (snd (ranges !! (idx - 1)) + 1)
 
+splitRanges :: [(Int, Int)] -> Int -> [(Int, Int)]
 splitRanges [] _ = []
 splitRanges ((start, end) : ranges) lastEnd = (max start (lastEnd + 1), end) : splitRanges ranges (max lastEnd end)
 
+numFreshIds :: ([(Int, Int)], a) -> Int
 numFreshIds (ranges, _) = sum [numInRange start end | (start, end) <- splitRanges (sortOn fst ranges) (-1)]
 
 inRange :: (Int, Int) -> Int -> Bool
