075ebdca7753d870dca96dc2c27ce601628e0f05
TheArchons <github@thearchons.xyz>
2025-12-31 11:04:49 -0500

    6
    might be the ugliest solution but it works

--- 2025/6/6.hs
+++ 2025/6/6.hs
@@ -1,11 +1,13 @@
-import Data.List (transpose)
+import Data.Char (digitToInt)
 
 main = do
   contents <- getContents
   print (getSum (parseContents contents))
 
-getSum :: [[String]] -> Int
-getSum questions = sum [processLine question | question <- questions]
+getSum :: [String] -> Int
+getSum lines
+  | null (head lines) = 0
+  | otherwise = solveQuestion lines (head (last lines)) + getSum (tailQuestion lines)
 
 processLine :: [String] -> Int
 processLine question =
@@ -15,8 +17,26 @@ processLine question =
   where
     nums = map read (init question)
 
-parseContents :: String -> [[String]]
-parseContents contents = transpose (map words (split contents '\n'))
+getFirstColNum :: [[Char]] -> Int
+getFirstColNum lines = foldl (\l r -> l * 10 + r) 0 [digitToInt currVal | idx <- [0 .. (length lines - 2)], let currVal = head (lines !! idx), currVal /= ' ']
+
+isEmptyCol :: [String] -> Bool
+isEmptyCol = all (\line -> head line == ' ')
+
+solveQuestion :: [String] -> Char -> Int
+solveQuestion lines operator
+  | null (head lines) || isEmptyCol lines = if operator == '+' then 0 else 1
+  | operator == '+' = getFirstColNum lines + solveQuestion (map tail lines) '+'
+  | operator == '*' = getFirstColNum lines * solveQuestion (map tail lines) '*'
+
+tailQuestion :: [String] -> [String]
+tailQuestion lines
+  | null (head lines) = lines
+  | isEmptyCol lines = map tail lines
+  | otherwise = tailQuestion (map tail lines)
+
+parseContents :: String -> [String]
+parseContents contents = split contents '\n'
 
 silentTail :: [a] -> [a]
 silentTail xs
